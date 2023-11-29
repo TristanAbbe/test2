3d5fa5e70790efdb10f8a7dfb75c7decc94278c9
Janelle ANET <jaaanelleannet@gmail.com>
2023-11-29 09:30:10 +0100

    chat

---
+++ CheshireCat.java
@@ -0,0 +1,33 @@
+
+/**
+ * Décrivez votre classe CheshireCat ici.
+ *
+ * @author (votre nom)
+ * @version (un numéro de version ou une date)
+ */
+public class CheshireCat
+{
+    // variables d'instance - remplacez l'exemple qui suit par le vôtre
+    private int x;
+
+    /**
+     * Constructeur d'objets de classe CheshireCat
+     */
+    public CheshireCat()
+    {
+        // initialisation des variables d'instance
+        x = 0;
+    }
+
+    /**
+     * Un exemple de méthode - remplacez ce commentaire par le vôtre
+     *
+     * @param  y   le paramètre de la méthode
+     * @return     la somme de x et de y
+     */
+    public int sampleMethod(int y)
+    {
+        // Insérez votre code ici
+        return x + y;
+    }
+}
