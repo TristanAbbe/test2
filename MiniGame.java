b52479138572e7658e9b6063a71dcb63ef84064a
Janelle ANET <jaaanelleannet@gmail.com>
2023-11-29 11:19:08 +0100

    jeu de dés

--- MiniGame.java
+++ MiniGame.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 
 /**
  * Décrivez votre classe MiniGame ici.
@@ -8,7 +9,7 @@
 public class MiniGame
 {
     // variables d'instance - remplacez l'exemple qui suit par le vôtre
-    private int x;
+    private int score;
 
     /**
      * Constructeur d'objets de classe MiniGame
@@ -16,9 +17,17 @@ public class MiniGame
     public MiniGame()
     {
         // initialisation des variables d'instance
-        x = 0;
+        score = 0;
     }
-
+    
+    public int getScore() {
+        return score;
+    }
+    
+    public void addToScore(int points) {
+        this.score += points;
+    }
+    
     /**
      *Dice game : queen of hearts and Alice play together
      *values of dice are random - Alice chooses the rules, she needs to lose to win the game
@@ -27,6 +36,21 @@ public class MiniGame
     public int DiceGame ()
     {
         // Insérez votre code ici
-        return x;
+        System.out.println("Bienvenue dans le jeu de dés !");
+        //Joueur joueur1 = new Joueur("Joueur 1");
+        //Joueur joueur2 = new Joueur("Joueur 2");
+        
+        //System.out.println(joueur.getNom() + ", appuyez sur Entrée pour lancer le dé.");
+
+        // Générer un nombre aléatoire entre 1 et 6 pour simuler le dé
+        Random random = new Random();
+        int result = random.nextInt(6) + 1;
+
+        //System.out.println(joueur.getNom() + " a obtenu un " + resultatDe + "!");
+        //joueur.ajouterAuScore(resultatDe);
+        
     }
 }
+
+
+
