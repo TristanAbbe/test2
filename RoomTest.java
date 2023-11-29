55e58b0cf8dfb73541e2ef61652f7b7a97c6f041
Mazurie Jules <jules.mazurie@gmail.com>
2023-11-29 12:09:56 +0100

    test room

--- RoomTest.java
+++ RoomTest.java
@@ -5,31 +5,61 @@ public class RoomTest {
 
     @Test
     public void testRoomCreation() {
-        Room myRoom = new Room("This is a room.", "Example Room");
-
-        assertEquals("Example Room", myRoom.getName());
-        assertEquals("This is a room.", myRoom.getDescription());
+        // Create a room and check its properties
+        Room room = new Room("This is a kitchen.", "Kitchen");
+        assertEquals("Kitchen", room.getName());
+        assertEquals("This is a kitchen.", room.getDescription());
     }
 
     @Test
-    public void testSetExit() {
-        Room room1 = new Room("Room 1", "First Room");
-        Room room2 = new Room("Room 2", "Second Room");
+    public void testSetAndGetExit() {
+        // Create two rooms
+        Room room1 = new Room("This is a living room.", "Living Room");
+        Room room2 = new Room("This is a bedroom.", "Bedroom");
 
+        // Set an exit from room1 to room2
         room1.setExit("north", room2);
 
+        // Check if the exit is set correctly
         assertEquals(room2, room1.getExit("north"));
     }
 
     @Test
     public void testGetExitString() {
-        Room room = new Room("Room with exits", "Exit Room");
-        Room neighbor1 = new Room("Neighbor 1", "First Neighbor");
-        Room neighbor2 = new Room("Neighbor 2", "Second Neighbor");
+        // Create a room and set exits
+        Room room = new Room("This is a hallway.", "Hallway");
+        Room roomNorth = new Room("This is the north room.", "North Room");
+        Room roomSouth = new Room("This is the south room.", "South Room");
 
-        room.setExit("north", neighbor1);
-        room.setExit("east", neighbor2);
+        room.setExit("north", roomNorth);
+        room.setExit("south", roomSouth);
 
-        assertEquals("Exits: north east", room.getExitString());
+        // Check if the string of exits is generated correctly
+        String expected = "Exits: north south";
+        assertEquals(expected, room.getExitString());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testSetNameWithNull() {
+        // Try setting name to null, should throw IllegalArgumentException
+        Room room = new Room("This is a test room.", null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testSetNameWithEmptyString() {
+        // Try setting name to an empty string, should throw IllegalArgumentException
+        Room room = new Room("This is a test room.", "");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testSetDescriptionWithNull() {
+        // Try setting description to null, should throw IllegalArgumentException
+        Room room = new Room(null, "Test Room");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testSetDescriptionWithEmptyString() {
+        // Try setting description to an empty string, should throw IllegalArgumentException
+        Room room = new Room("", "Test Room");
     }
 }
