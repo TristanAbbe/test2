5132cf384888e4cba502b29f52b9ed85587f5e59
Corentin Delpirou <delpirou.corentin@gmail.com>
2023-11-29 10:04:01 +0100

    MaJ commentaires

--- Alice.java
+++ Alice.java
@@ -34,6 +34,4 @@ public class Alice extends Character
     public void interactWithNPC(NPC npc) {
         // Code pour interagir avec les personnages non-jouables
     }
-
-    // Autres méthodes et propriétés nécessaires
 }
